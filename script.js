676a720cdfd4aabb1e2e763c0b306f3b1b314531
Joshua Franz <76621974+Joshua-Franz-Vollkornbyte@users.noreply.github.com>
2023-09-09 15:18:35 +0200

    Quizstartseite, Unterseiten

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
 const questions = [
     "Welche Art von rechtlichen Dienstleistungen benötigen Sie?",
-    "In welchem Rechtsgebiet benötigen Sie Unterstützung?",
-    "Wie groß ist Ihr Unternehmen?",
     // Fügen Sie hier weitere Fragen hinzu
-];
-
-const companies = [
-    "Firma 1",
-    "Firma 2",
-    "Firma 3",
-    "Firma 4",
-    // Fügen Sie hier weitere Firmen hinzu
 ];
 
 let currentQuestionIndex = 0;
 
-document.getElementById("next-button").addEventListener("click", function() {
+document.getElementById("next-button").addEventListener("click", function () {
     const selectedAnswer = document.querySelector('input[name="answer"]:checked');
-    
+
     if (selectedAnswer) {
         const answerValue = selectedAnswer.value;
         // Speichern Sie die Antwort oder bewerten Sie sie nach Ihren Kriterien.
-        
+
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             displayQuestion();
         } else {
-            displayResult(answerValue);
+            reffer(answerValue);
         }
 
         selectedAnswer.checked = false; // Zurücksetzen der Auswahl
@@ -34,22 +24,26 @@ document.getElementById("next-button").addEventListener("click", function() {
 });
 
 function displayQuestion() {
-    const questionText = document.getElementById("question-text");
-    questionText.textContent = `Frage ${currentQuestionIndex + 1}: ${questions[currentQuestionIndex]}`;
+    // Hier sollten Sie den Code hinzufügen, um die Frage anzuzeigen
 }
 
-function displayResult(answerValue) {
-    const resultText = document.getElementById("result-text");
-    const recommendedCompany = document.getElementById("recommended-company");
-    
-    resultText.textContent = "Basierend auf Ihren Antworten empfehlen wir Ihnen folgende Firma:";
-    recommendedCompany.textContent = answerValue; // Hier können Sie die Empfehlung basierend auf den Benutzerantworten generieren.
-
-    const answersDiv = document.getElementById("answers");
-    answersDiv.style.display = "none";
-    
-    const resultsDiv = document.getElementById("results");
-    resultsDiv.style.display = "block";
+function reffer(answerValue) {
+    // Automatische Weiterleitung nach 5 Sekunden
+    if (answerValue === "1") {
+        setTimeout(function () {
+            window.location.href = "R1.html";
+        }, 1000);
+    } else if (answerValue === "2") {
+        setTimeout(function () {
+            window.location.href = "R2.html";
+        }, 1000);
+    } else if (answerValue === "3") {
+        setTimeout(function () {
+            window.location.href = "R3.html";
+        }, 1000);
+    } else if (answerValue === "4") {
+        setTimeout(function () {
+            window.location.href = "R4.html";
+        }, 1000);
+    }
 }
-
-displayQuestion();
